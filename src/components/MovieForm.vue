cddb21a60fa09828229c18fbf49af71dea5c2c3d
GBrayJ <brayden@braydenjohnson.dev>
2023-02-16 10:19:44 -0700

    Added Working Form

--- src/components/MovieForm.vue
+++ src/components/MovieForm.vue
@@ -1,14 +1,32 @@
 <template>
-  <form>
-<input v-bind="this.moviename" type="text" placeholder="Movie Name" />
-<input v-bind="this.year" type="number" placeholder="Year" />
-<input v-bind="this.description" type="text" placeholder="Description" />
-<input v-bind="this.imageURL" type="url" placeholder="Image URL" />
-<input v-bind="this.genre" type="text" placeholder="Genre" />
-<input v-bind="this.rating" type="number" placeholder="Rating" />
-<input v-bind="this.director" type="text" placeholder="Director" />
-<input v-bind="this.actors" type="text" placeholder="Actors" />
-    <input type="submit" value="Submit" @click="submitForm"/>
+  <form name="movieForm" onsubmit="return false;" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <input
+        class="m-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        name="movieName" type="text" placeholder="Movie Name" required/>
+    <input
+        class="m-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        name="year" type="number" placeholder="Year" required/>
+    <input
+        class="m-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        name="description" type="text" placeholder="Description" required/>
+    <input
+        class="m-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        name="imageURL" type="url" placeholder="Image URL"/>
+    <input
+        class="m-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        name="genre" type="text" placeholder="Genre" required/>
+    <input
+        class="m-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        name="rating" type="number" placeholder="Rating" required/>
+    <input
+        class="m-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        name="director" type="text" placeholder="Director"/>
+    <input
+        class="m-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        name="actors" type="text" placeholder="Actors"/>
+    <input
+        class="m-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        type="submit" value="Submit" @click="submitForm"/>
   </form>
 </template>
 
@@ -16,6 +34,7 @@
 export default {
   name: "MovieForm",
   data: () => ({
+    movieObject: {
       moviename: "",
       year: "",
       description: "",
@@ -24,10 +43,27 @@ export default {
       rating: "",
       director: "",
       actors: ""
+    }
   }),
   methods: {
     submitForm() {
-
+      if (isNaN(document.movieForm.rating.value) || document.movieForm.rating.value < 1 || document.movieForm.rating.value > 10) {
+        alert("Rating must be a number between 1 and 10");
+        return false;
+      }
+      if (isNaN(document.movieForm.year.value) || document.movieForm.year.value < 1900 || document.movieForm.year.value > 2021) {
+        alert("Year must be a number between 1900 and 2021");
+        return false;
+      }
+      this.movieObject.moviename = document.movieForm.movieName.value;
+      this.movieObject.year = document.movieForm.year.value;
+      this.movieObject.descriptiontext = document.movieForm.description.value;
+      this.movieObject.imageURL = document.movieForm.imageURL.value;
+      this.movieObject.genre = document.movieForm.genre.value;
+      this.movieObject.rating = document.movieForm.rating.value;
+      this.movieObject.director = document.movieForm.director.value;
+      this.movieObject.actors = document.movieForm.actors.value;
+      this.$emit("submitClicked", this.movieObject);
     }
   }
 }
--- src/views/submit.vue
+++ src/views/submit.vue
@@ -1,13 +1,48 @@
 <template>
-<movie-form></movie-form>
+  <movie-form @submitClicked="submitForm"></movie-form>
 </template>
 
 <script>
 import MovieForm from "../components/MovieForm.vue";
 
+import axios from "axios";
+
 export default {
   name: "submit",
-  components: {MovieForm}
+  components: {MovieForm},
+  data() {
+    return {
+      movie: {},
+    };
+  },
+  methods: {
+    submitForm(movie) {
+      let options = {
+        method: 'POST',
+        url: 'https://moviebrowserbackend.onrender.com/data/movie/new',
+        headers: {'Content-Type': 'application/json'},
+        data: {
+          name: movie.moviename,
+          descriptiontext: movie.descriptiontext,
+          rating: movie.rating,
+          imageurl: movie.imageurl,
+          year: movie.year,
+          genre: movie.genre,
+          director: movie.director,
+          actors: movie.actors
+        }
+      };
+      axios.request(options).then(function (response) {
+        if(response.data.objectID) {
+          window.location = "/#/movies/" + response.data.objectID
+        } else {
+          alert(response.data)
+        }
+      }).catch(function (error) {
+        console.error(error);
+      });
+    }
+  }
 }
 </script>
 
